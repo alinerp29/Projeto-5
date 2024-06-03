a82127352123f17ab78a0a911919447abb8f36b6
alinerp29 <149211403+alinerp29@users.noreply.github.com>
2024-06-03 12:55:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ car_data = pd.read_csv('vehicles_us.csv')
 st.header('Quatro rodas', divider='rainbow') 
 st.header ('Conheça mais sobre nossos veículos')
 
-hist_button = st.button('Conheça mais sobre nossos veículos que passaram por nossas lojas.') # criar um botão
+hist_button = st.button('Confira quantos dias os carros permanecem disponíveis em nossa loja.') # criar um botão
 if hist_button: # se o botão for clicado
     fig = px.histogram(car_data, x="days_listed")
     st.plotly_chart(fig, use_container_width=True)
 
 
-hist_button = st.button('Grandes oportunidade!  Vejam os carros questão a venda em nossa loja.') # criar um botão
+hist_button = st.button('Grandes oportunidade!  Veja a relação de carros disponíveis considerando valor x ano de fabricação.') # criar um botão
 if hist_button:     
     fig = px.scatter(car_data, x="model_year", y="price") # criar um gráfico de dispersão
     fig.show() # exibindo
